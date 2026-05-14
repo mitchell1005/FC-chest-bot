151bdc200fa36e2deaa0bc95889514b8d3152a37
Mitchell Sterrenberg <mitchellsterrenberg@gmail.com>
2026-05-14 18:01:22 +0200

    add new preset

--- src/presets.js
+++ src/presets.js
@@ -3,41 +3,58 @@
 
 const PRESETS = {
   'submarine-shark': {
-    name: 'Submarine - Shark',
-    items: [
-      { item_name: 'Coke', quantity_needed: 8 },
-      { item_name: 'Mythril Plate', quantity_needed: 4 },
-      { item_name: 'Electrum Ingot', quantity_needed: 4 },
-      { item_name: 'Walnut Lumber', quantity_needed: 4 },
-    ]
-  },
-  'submarine-unkiu': {
-    name: 'Submarine - Unkiu',
-    items: [
-      { item_name: 'Titanium Alloy Mirror', quantity_needed: 8 },
-      { item_name: 'Hardsilver Ingot', quantity_needed: 12 },
-      { item_name: 'Teak Lumber', quantity_needed: 8 },
-      { item_name: 'Gaganaskin Strap', quantity_needed: 4 },
-    ]
-  },
-  'submarine-syldra': {
-    name: 'Submarine - Syldra',
-    items: [
-      { item_name: 'Hallowed Chestnut Lumber', quantity_needed: 8 },
-      { item_name: 'Chromite Ingot', quantity_needed: 12 },
-      { item_name: 'Dwarven Cotton Yarn', quantity_needed: 8 },
-      { item_name: 'Palladium Ingot', quantity_needed: 8 },
-    ]
-  },
-  'submarine-modified-shark': {
-    name: 'Submarine - Modified Shark',
-    items: [
-      { item_name: 'Cobalt Ingot', quantity_needed: 12 },
-      { item_name: 'Mythril Plate', quantity_needed: 8 },
-      { item_name: 'Electrum Ingot', quantity_needed: 8 },
-      { item_name: 'Walnut Lumber', quantity_needed: 8 },
-    ]
-  },
+  name: 'Submarine - Shark',
+  items: [
+    // Hull
+    { item_name: 'Hull - Walnut Lumber', quantity_needed: 18 },
+    { item_name: 'Hull - Spruce Lumber', quantity_needed: 18 },
+    { item_name: 'Hull - Iron Nails', quantity_needed: 18 },
+    { item_name: 'Hull - Cobalt Ingot', quantity_needed: 18 },
+    { item_name: 'Hull - Cedar Lumber', quantity_needed: 9 },
+    { item_name: 'Hull - Iron Rivets', quantity_needed: 18 },
+    { item_name: 'Hull - Mythril Plate', quantity_needed: 18 },
+    { item_name: 'Hull - Electrum Ingot', quantity_needed: 18 },
+    { item_name: 'Hull - Horn Glue', quantity_needed: 18 },
+    { item_name: 'Hull - Mythril Rivets', quantity_needed: 18 },
+    { item_name: 'Hull - Rose Gold Ingot', quantity_needed: 21 },
+    { item_name: 'Hull - Varnish', quantity_needed: 18 },
+    // Stern
+    { item_name: 'Stern - Steel Rivets', quantity_needed: 24 },
+    { item_name: 'Stern - Cobalt Ingot', quantity_needed: 18 },
+    { item_name: 'Stern - Steel Ingot', quantity_needed: 24 },
+    { item_name: 'Stern - Cobalt Joint Plate', quantity_needed: 24 },
+    { item_name: 'Stern - Steel Ingot', quantity_needed: 30 },
+    { item_name: 'Stern - Darksteel Ingot', quantity_needed: 9 },
+    { item_name: 'Stern - Steel Joint Plate', quantity_needed: 30 },
+    { item_name: 'Stern - Cobalt Ingot', quantity_needed: 30 },
+    { item_name: 'Stern - Ancient Lumber', quantity_needed: 9 },
+    { item_name: 'Stern - Mythril Rivets', quantity_needed: 30 },
+    { item_name: 'Stern - Cobalt Ingot', quantity_needed: 30 },
+    { item_name: 'Stern - Silver Ingot', quantity_needed: 30 },
+    // Bow
+    { item_name: 'Bow - Steel Ingot', quantity_needed: 30 },
+    { item_name: 'Bow - Steel Rivets', quantity_needed: 30 },
+    { item_name: 'Bow - Cobalt Joint Plate', quantity_needed: 30 },
+    { item_name: 'Bow - Cobalt Ingot', quantity_needed: 18 },
+    { item_name: 'Bow - Steel Rivets', quantity_needed: 30 },
+    { item_name: 'Bow - Steel Joint Plate', quantity_needed: 30 },
+    { item_name: 'Bow - Darksteel Ingot', quantity_needed: 12 },
+    { item_name: 'Bow - Ancient Lumber', quantity_needed: 9 },
+    { item_name: 'Bow - Mythril Rivets', quantity_needed: 30 },
+    { item_name: 'Bow - Silver Ingot', quantity_needed: 30 },
+    // Bridge
+    { item_name: 'Bridge - Treated Spruce Lumber', quantity_needed: 12 },
+    { item_name: 'Bridge - Darksteel Nugget', quantity_needed: 21 },
+    { item_name: 'Bridge - Iron Nails', quantity_needed: 21 },
+    { item_name: 'Bridge - Iron Rivets', quantity_needed: 21 },
+    { item_name: 'Bridge - Steel Ingot', quantity_needed: 21 },
+    { item_name: 'Bridge - Steel Rivets', quantity_needed: 21 },
+    { item_name: 'Bridge - Darksteel Ingot', quantity_needed: 24 },
+    { item_name: 'Bridge - Cobalt Rivets', quantity_needed: 21 },
+    { item_name: 'Bridge - Cobalt Ingot', quantity_needed: 18 },
+    { item_name: 'Bridge - Electrum Ingot', quantity_needed: 21 },
+  ]
+},
 };
 
 function getPresetNames() {
